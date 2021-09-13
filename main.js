b80af4e5429dab39b33ee7a89d0c1f9f27b3f4cb
Zapzor <imp.4xlbm@aleeas.com>
2021-09-13 22:10:42 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const gameBoard = (() => {
+ const board = []
+ const renderBoard = function() {
+   foo = document.querySelector('.boardContainer')
+
+ }
+})()
+
+const displayController = (() => {
+
+
+})()
+
+const Player = () => {
+
+}
