3aab52763ac7eaaae18f3737c523da3069634988
Zapzor <imp.4xlbm@aleeas.com>
2021-09-14 05:43:03 +0100

    Completed project. Need to refactor code.

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const gameBoard = (() => {
- const board = []
- const renderBoard = function() {
-   foo = document.querySelector('.boardContainer')
 
- }
-})()
+})();
 
 const displayController = (() => {
-
 
 })()
 
 const Player = () => {
 
+  return {}
 }
+
+const cell = document.querySelectorAll('.cell')
+
+const xTurn = 'X'
+const oTurn = 'O'
+
+const changeCell = (e) => {
+  foo = e
+  cell.forEach((e) => {
+    e.classList.toggle('turnChange')
+    if  (e.classList.contains('turnChange')) {
+      foo.target.innerText = 'X'
+    } else
+    foo.target.innerText = 'O'
+  })
+
+}
+
+cell.forEach(cell => {
+  cell.addEventListener('click', changeCell, {once: true})
+})
