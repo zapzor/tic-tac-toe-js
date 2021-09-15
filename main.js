ce7ebe1bbc26846d725cdd9eb66637931cd90d40
Zapzor <imp.4xlbm@aleeas.com>
2021-09-15 21:52:53 +0100

    Updated logic of game state checker. Improved code styling

--- main.js
+++ main.js
@@ -1,153 +1,103 @@
 // Handles game logic
 const gameBoard = (() => {
-
   // DOM handling and array for determining game length
-  const cell = document.querySelectorAll('.cell')
-  const declareWinner = document.querySelector('.declareWinner')
-  const declareWinnerText = document.querySelector('.declareWinnerText')
-  const currentTurn = document.createElement('div')
-  let gameArray = []
+  const boardCells = document.querySelectorAll('.cell');
+  const declareWinner = document.querySelector('.declareWinner');
+  const declareWinnerText = document.querySelector('.declareWinnerText');
+  const currentTurn = document.createElement('div');
+  let gameArray = [];
 
   // Selectors for individual board cells
-  const div1 = document.querySelector('.div1')
-  const div2 = document.querySelector('.div2')
-  const div3 = document.querySelector('.div3')
-  const div4 = document.querySelector('.div4')
-  const div5 = document.querySelector('.div5')
-  const div6 = document.querySelector('.div6')
-  const div7 = document.querySelector('.div7')
-  const div8 = document.querySelector('.div8')
-  const div9 = document.querySelector('.div9')
 
   // Messages to be displayed on game end
   const xWinner = () => {
-    declareWinnerText.textContent = 'X WINS'
-  }
+    declareWinnerText.textContent = 'X WINS';
+  };
 
   const oWinner = () => {
-    declareWinnerText.textContent = 'O WINS'
-  }
+    declareWinnerText.textContent = 'O WINS';
+  };
 
   const draw = () => {
-    declareWinnerText.textContent = 'Draw'
-  }
+    declareWinnerText.textContent = 'Draw';
+  };
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  const checkWinner = (text) => {
+    return winConditions.some((combos) => {
+      return combos.every((index) => {
+        return boardCells[index].textContent.includes(text);
+      });
+    });
+  };
 
   //Checks for win, loss or draw according to cells text content
-  const checkArray = () => {
+  const announceWinner = () => {
     if (gameArray.length === 9) {
-      declareWinner.classList.add('active')
-      draw()
+      declareWinner.classList.add('active');
+      draw();
+    } else if (checkWinner('X')) {
+      declareWinner.classList.add('active');
+      xWinner();
+    } else if (checkWinner('O')) {
+      declareWinner.classList.add('active');
+      oWinner();
     }
-    else if (div1.textContent === 'X' && div2.textContent === 'X' && div3.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div4.textContent === 'X' && div5.textContent === 'X' && div6.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div7.textContent === 'X' && div8.textContent === 'X' && div9.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div1.textContent === 'X' && div5.textContent === 'X' && div9.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div3.textContent === 'X' && div5.textContent === 'X' && div7.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div1.textContent === 'X' && div4.textContent === 'X' && div7.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div2.textContent === 'X' && div5.textContent === 'X' && div8.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div3.textContent === 'X' && div6.textContent === 'X' && div9.textContent === 'X') {
-      declareWinner.classList.toggle('active')
-      xWinner()
-    }
-    else if (div1.textContent === 'O' && div2.textContent === 'O' && div3.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div4.textContent === 'O' && div5.textContent === 'O' && div6.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div7.textContent === 'O' && div8.textContent === 'O' && div9.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div1.textContent === 'O' && div5.textContent === 'O' && div9.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div3.textContent === 'O' && div5.textContent === 'O' && div7.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div1.textContent === 'O' && div4.textContent === 'O' && div7.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div2.textContent === 'O' && div5.textContent === 'O' && div8.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-    else if (div3.textContent === 'O' && div6.textContent === 'O' && div9.textContent === 'O') {
-      declareWinner.classList.toggle('active')
-      oWinner()
-    }
-  }
+  };
   return {
     declareWinner,
     gameArray,
-    cell,
+    boardCells,
     currentTurn,
-    checkArray
-  }
-
+    announceWinner,
+  };
 })();
 
 // Handles visuals
 const displayController = (() => {
-
-  const restartButton = document.querySelector('.restartButton')
+  const restartButton = document.querySelector('.restartButton');
 
   //Handles marker changing on mouseclick
   const changeMarker = (e) => {
-    gameBoard.currentTurn.classList.toggle('turnChange')
+    gameBoard.currentTurn.classList.toggle('turnChange');
     if (gameBoard.currentTurn.classList.contains('turnChange')) {
-      e.target.textContent = 'X'
-      gameBoard.gameArray.push('X')
+      e.target.textContent = 'X';
+      gameBoard.gameArray.push('X');
+      gameBoard.announceWinner();
     } else {
-      e.target.innerText = 'O'
-      gameBoard.gameArray.push('O')
+      e.target.innerText = 'O';
+      gameBoard.gameArray.push('O');
+      gameBoard.announceWinner();
     }
-  }
+  };
 
   const handleClick = () => {
-    gameBoard.cell.forEach(cell => {
-      cell.addEventListener('click', changeMarker, {once: true}),
-      cell.addEventListener('click', gameBoard.checkArray)
-    })
-  }
-  handleClick()
+    gameBoard.boardCells.forEach((cell) => {
+      cell.addEventListener('click', changeMarker, { once: true });
+    });
+  };
+  handleClick();
 
   // Clears board and array on clicking button
   const clearBoard = () => {
-    handleClick()
-    gameBoard.currentTurn.classList.remove('turnChange')
-    gameBoard.declareWinner.classList.remove('active')
-    gameBoard.gameArray = []
+    handleClick();
+    gameBoard.currentTurn.classList.remove('turnChange');
+    gameBoard.declareWinner.classList.remove('active');
+    gameBoard.gameArray = [];
 
-    gameBoard.cell.forEach (cell => {
-      cell.textContent = ''
-    })
-  }
-  restartButton.addEventListener('click', clearBoard)
-})()
+    gameBoard.boardCells.forEach((cell) => {
+      cell.textContent = '';
+    });
+  };
+  restartButton.addEventListener('click', clearBoard);
+  return {};
+})();
